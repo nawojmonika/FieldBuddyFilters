aa335b447c57fc2ad6427da3b2af292e3dd5109b
nawojmonika <nawojmonika@gmail.com>
2020-10-09 23:17:19 +0200

    Added getting orders on list mount and rendering them

--- components/orderList/OrderList.ts.tsx
+++ components/orderList/OrderList.ts.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
-import {Text, View} from "react-native";
+import {StyleSheet, Text, View} from "react-native";
+import {OrderListState} from "./interfaces/OrderListState";
+import {OrderProps} from "../order/interfaces/OrderProps";
 
-export default class OrderList extends React.Component {
+
+export default class OrderList extends React.Component<unknown, OrderListState> {
     constructor(props: Readonly<unknown>) {
         super(props);
-        this.state = {date: new Date()};
+        this.state = {list: []};
+    }
+
+    async componentDidMount() {
+        try {
+            const list = require('../../assets/mockData/order-list.json');
+            this.setState({list})
+        }
+        catch (e) {
+            console.error(e)
+        }
     }
 
     render() {
         return (
-            <View>
-                <Text>This is an order list</Text>
+            <View style={styles.list}>
+                {this.state.list.map((element: OrderProps, index: number) => {
+                    return <Text key={index}>This is an order item!</Text>
+                })}
+
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    list: {
+        flex: 1
+    },
+});
---
+++ components/orderList/interfaces/OrderListState.ts
@@ -0,0 +1,5 @@
+import {OrderProps} from "../../order/interfaces/OrderProps";
+
+export interface OrderListState {
+    list: OrderProps[];
+}
